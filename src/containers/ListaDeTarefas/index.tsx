c85df758b913d3cdc2b693042f5967cd5d258f43
Douglas Eduardo <douglas.patusco@gmail.com>
2023-09-28 16:11:15 -0300

    Cards criados e parametrizados. Criados os estados de trocas de botões. Aplicação de Enums. Configuração do Redux

--- src/containers/ListaDeTarefas/index.tsx
+++ src/containers/ListaDeTarefas/index.tsx
@@ -1,7 +1,32 @@
-const ListaDeTarefas = () => (
-  <main>
-    <p>2 tarefas marcadas como: &quot;categoria&ldquo; e &quot;termo&ldquo;</p>
-  </main>
-)
+import { useSelector } from 'react-redux/'
+
+import Tarefa from '../../components/Tarefa'
+import { Container } from './styles'
+import * as enums from '../../utils/enums/Tarefa'
+import { RootReducer } from '../../store'
+
+const ListaDeTarefas = () => {
+  const { tarefas } = useSelector((state: RootReducer) => state)
+
+  return (
+    <Container>
+      <p>
+        2 tarefas marcadas como: &quot;categoria&ldquo; e &quot;termo&ldquo;
+      </p>
+      <ul>
+        {tarefas.map((t) => (
+          <li key={t.titulo}>
+            <Tarefa
+              titulo={t.titulo}
+              descricao={t.descricao}
+              prioridade={t.prioridade}
+              status={t.status}
+            />
+          </li>
+        ))}
+      </ul>
+    </Container>
+  )
+}
 
 export default ListaDeTarefas
